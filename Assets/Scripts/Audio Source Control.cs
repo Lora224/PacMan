418a243244eaff43e0364d489ef20de75f8eebfc
Lora Lyu <martialmask@163.com>
2024-10-01 19:30:54 +1000

    Added audio sources and control

---
+++ Assets/Scripts/Audio Source Control.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+
+public class AudioSourceControl : MonoBehaviour
+{
+   public AudioSource bgm;
+   public AudioSource ghostNormal;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!bgm.isPlaying)
+        {
+            ghostNormal.Play();
+        }
+    }
+}
