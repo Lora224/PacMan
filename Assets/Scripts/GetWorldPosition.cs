7d7beb536d863c9648913a7e7ac17fd8d1604f13
Lora Lyu <martialmask@163.com>
2024-10-04 16:53:35 +1000

    Created PacStudent Movement that controlls the circle movement of PacMan

---
+++ Assets/Scripts/GetWorldPosition.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+
+
+public class GetWorldPosition : MonoBehaviour
+{
+    public Tilemap tilemap;  
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Example grid position for the tile
+        Vector3Int gridPosition1 = new Vector3Int(-21, 13, 0);
+        Vector3Int gridPosition2 = new Vector3Int(-16, 13, 0);
+        Vector3Int gridPosition3 = new Vector3Int(-16, 9, 0);
+        Vector3Int gridPosition4 = new Vector3Int(-21, 9, 0);
+
+
+        // Convert the grid position to world position
+        Vector3 worldPosition1 = tilemap.CellToWorld(gridPosition1);
+        Vector3 worldPosition2 = tilemap.CellToWorld(gridPosition2);
+        Vector3 worldPosition3 = tilemap.CellToWorld(gridPosition3);
+        Vector3 worldPosition4 = tilemap.CellToWorld(gridPosition4);
+        // Log the world position to the console
+        Debug.Log("World Position of Tile: " + worldPosition1);
+        Debug.Log("World Position of Tile: " + worldPosition2);
+        Debug.Log("World Position of Tile: " + worldPosition3);
+        Debug.Log("World Position of Tile: " + worldPosition4);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
+
--- Assets/Scripts/PacStudent Movement.cs
+++ Assets/Scripts/PacStudent Movement.cs
@@ -1,18 +1,80 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Tilemaps;
 
-public class Pac : MonoBehaviour
+public class PacStudentMovement : MonoBehaviour
 {
+
+    Animator pacStudentAnimator;
+    AudioSource movementAudio;
+    public bool IsMoving;
+    public float speed;
+    GameObject PacStudent;
+    Tilemap tile;
+
+    Vector3[] corners = new Vector3[]
+{
+    new Vector3(-10.2f, 6.7f, 0), // Top-left
+    new Vector3(-7.8f, 6.7f, 0), // Top-right
+    new Vector3(-7.8f, 4.7f, 0), // Bottom-right
+    new Vector3(-10.2f, 4.7f, 0)  // Bottom-left
+};
+
+
+    int currentCorner = 0;
+    private void MovePacStudent()
+    {
+        Vector3 targetPosition = corners[currentCorner];
+        PacStudent.transform.position = Vector3.MoveTowards(PacStudent.transform.position, targetPosition, speed * Time.deltaTime);
+
+        if (Vector3.Distance(PacStudent.transform.position, targetPosition) < 0.1f)
+        {
+            if (currentCorner != 0)
+            {
+                pacStudentAnimator.SetBool("turned", true);
+            }
+            currentCorner = (currentCorner + 1) % corners.Length;
+        }
+    }
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        pacStudentAnimator = GetComponent<Animator>();
+        pacStudentAnimator.SetTrigger("Start");
+        movementAudio = GetComponent<AudioSource>();
+        pacStudentAnimator.SetBool("turned", false);
+        PacStudent = this.gameObject;
+
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        pacStudentAnimator.SetBool("turned", false);
+        MovePacStudent();
+
+
+        if (PacStudentIsMoving())
+        {
+            pacStudentAnimator.SetBool("IsMoving", true);
+            if (!movementAudio.isPlaying)
+            {
+                movementAudio.Play();
+            }
+        }
+        else
+        {
+            pacStudentAnimator.SetBool("IsMoving", false);
+            movementAudio.Stop();
+        }
     }
+
+    bool PacStudentIsMoving()
+    {
+        return PacStudent.transform.position != corners[currentCorner];
+    }
+
 }
